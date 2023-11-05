35f3a897aa4b4dffbd3f9a8a5eb1933424ec6d05
nukeykt <alexeytf2@gmail.com>
2023-11-06 07:19:09 +0900

    prescaler

--- fmopl3.c
+++ fmopl3.c
@@ -3,5 +3,67 @@
 
 void FMOPL3_Clock(fmopl3_t *chip)
 {
+    chip->mclk1 = !chip->input.mclk;
+    chip->mclk2 = chip->input.mclk;
 
+    if (chip->mclk1)
+    {
+        chip->prescaler1_reset[0] = (chip->prescaler1_reset[1] << 1) | chip->reset1;
+
+        if (!chip->prescaler1_reset[1] && chip->reset1)
+            chip->prescaler1_cnt[0] = 0;
+        else
+            chip->prescaler1_cnt[0] = (chip->prescaler1_cnt[1] + 1) & 3;
+    }
+    if (chip->mclk2)
+    {
+        chip->prescaler1_reset[1] = chip->prescaler1_reset[0];
+        chip->prescaler1_cnt[1] = chip->prescaler1_cnt[0];
+    }
+
+    chip->prescaler1_clk = chip->tm_w1 ? chip->input.mclk : !(chip->prescaler1_cnt[1] & 2);
+
+    chip->aclk1 = !chip->prescaler1_clk;
+    chip->aclk2 = chip->prescaler1_clk;
+
+    if (chip->aclk1)
+    {
+        chip->prescaler2_reset_l[0] = (chip->prescaler2_reset_l[1] << 1) | chip->reset1;
+
+        if (prescaler2_reset)
+            chip->prescaler2_cnt[0] = 0;
+        else
+            chip->prescaler2_cnt[0] = (chip->prescaler2_cnt[1] + 1) & 3;
+
+        chip->prescaler2_l1[0] = !chip->prescaler2_reset && (chip->prescaler2_cnt[1] & 1) == 0;
+        chip->prescaler2_l2 = chip->prescaler2_l1[1];
+
+        chip->prescaler2_l3[0] = !chip->prescaler2_reset && (chip->prescaler2_cnt[1] & 1) != 0;
+        chip->prescaler2_l4 = chip->prescaler2_l3[1];
+
+        chip->prescaler2_l5[0] = !chip->prescaler2_reset && chip->prescaler2_cnt[1] == 3;
+
+        chip->prescaler2_l6[0] = !chip->prescaler2_reset && chip->prescaler2_cnt[1] == 1;
+
+        chip->prescaler2_l7 = (chip->prescaler2_cnt[1] & 1) == 0;
+    }
+    if (chip->aclk2)
+    {
+        chip->prescaler2_reset_l[1] = chip->prescaler2_reset_l[0];
+        chip->prescaler2_cnt[1] = chip->prescaler2_cnt[0];
+        chip->prescaler2_l1[1] = chip->prescaler2_l1[0];
+        chip->prescaler2_l3[1] = chip->prescaler2_l3[0];
+        chip->prescaler2_l5[1] = chip->prescaler2_l5[0];
+        chip->prescaler2_l6[1] = chip->prescaler2_l6[0];
+    }
+
+    chip->prescaler2_reset = !chip->prescaler2_reset_l[1] && chip->reset1;
+
+    chip->clk1 = chip->prescaler2_l1[1] && !chip->prescaler2_l2;
+    chip->clk2 = chip->prescaler2_l3[1] && !chip->prescaler2_l4;
+
+    chip->rclk1 = chip->prescaler2_l6[1];
+    chip->rclk2 = chip->prescaler2_l5[1];
+
+    chip->bclk = !chip->prescaler2_reset && chip->prescaler2_l7 && (chip->prescaler2_cnt[1] & 1) == 0;
 }
--- fmopl3.h
+++ fmopl3.h
@@ -14,5 +14,39 @@ typedef struct
 typedef struct
 {
     fmopl3_input_t input;
+
+    int mclk1;
+    int mclk2;
+    int aclk1;
+    int aclk2;
+    int clk1;
+    int clk2;
+    int rclk1;
+    int rclk2;
+    int bclk;
+
+    int prescaler1_reset[2];
+    int prescaler1_cnt[2];
+
+    int prescaler1_clk;
+
+    int prescaler2_reset_l[2];
+    int prescaler2_cnt[2];
+    int prescaler2_reset;
+    int prescaler2_l1[2];
+    int prescaler2_l2;
+    int prescaler2_l3[2];
+    int prescaler2_l4;
+    int prescaler2_l5[2];
+    int prescaler2_l6[2];
+    int prescaler2_l7;
+
+    int reset1;
+
+
+    int tm_w1;
+    int tm_w2;
+    int tm_w3;
+    int tm_w4;
 } fmopl3_t;
 
